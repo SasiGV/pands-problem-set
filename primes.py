11c10f3c32a2df152f650100bd9293325fee397c
Sasikala <g00376470@gmit.ie>
2019-03-30 16:39:46 +0000

    Added Primes Program

---
+++ primes.py
@@ -0,0 +1,21 @@
+#Program to verify whether the input number is prime or not
+#Prime number is divisable by itself and 1 only
+
+#Get the input value from the user
+n = int(input("Please enter a positive number: "))
+
+#Prime numbers are greater than one so verify it
+if n > 1:
+    #Check and any other divisor
+    for i in range(2, n):
+        if(n % i) == 0:
+            print(int(n), "is not a Prime Number")
+            print(i, "times", int(n/i), "is", n)
+            break
+        else:
+            print(n, "is a Prime Number")
+            break
+#If input value is less than 0 or equal to 1 then it is not a Prime number
+else:
+    print(n,"is not a Prime Number")
+
