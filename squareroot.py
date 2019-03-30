e735103c449b909f25ab476af58d80924f508603
Sasikala <g00376470@gmit.ie>
2019-03-30 17:37:46 +0000

    Updated Squareroot program

--- squareroot.py
+++ squareroot.py
@@ -5,7 +5,15 @@ import math
 #Get input from the user
 n = float(input("Please enter a positive number: "))
 
+#Find Square root of the input value using math.sqrt method
 sq_root = math.sqrt(n)
-sq_root = format(sq_root, '.2f')
-print ("The square root of", n, "is approx. ", sq_root )
 
+#If the output is float value then format it to 2 decimal place
+if (sq_root.is_integer() == False):
+    sq_root = format(sq_root, '.2f')
+#if the output is not a float value then return a whole number 
+else:
+    sq_root = int(sq_root)
+    n = int(n)
+
+print ("The square root of", n, "is approx. ", sq_root )
