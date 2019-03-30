50b682712b604ccba30899fe29f5c9c0afa10e70
Sasikala <g00376470@gmit.ie>
2019-03-30 16:25:27 +0000

    Added comments to Collatz program

--- collatz.py
+++ collatz.py
@@ -2,15 +2,21 @@
 #If I/P value is even then divid it by 2 or if it is odd then mutiply by 3 and add one.
 #Continue to print until value is one
 
+#Get input from user
 n = int(input("Please enter a positive integer: "))
 
+#Verify the input whether it is Positive or not
 if n > 0:
-    #print out put
+    #Start to print out put
+    #end = " " will print the next value with a space instead of a new line
     print(n, end = " " )
+    #Start the loop and print the output unitl n becomes value 1
     while n > 1:
+        #If n in even then divide it by 2
         if (n % 2 == 0):
             n = n/ 2
             print(int(n), end = " " )
+        #if n is odd then multiply by 3 and add 1
         else:
             n = (n * 3) + 1
             print(int(n), end = " " )
