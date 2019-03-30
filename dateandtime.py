cfae2d34d36b2e4055003fd941228424b2848ad8
Sasikala <g00376470@gmit.ie>
2019-03-30 17:56:10 +0000

    Added DateandTime Program

---
+++ dateandtime.py
@@ -0,0 +1,14 @@
+#Program to print date time in the given format
+#Monday, January 10th 2019 at 1.15pm
+
+#Import date method from datetime module
+from datetime import datetime
+from datetime import time
+
+today = datetime.today()
+now = datetime.now()
+print (datetime.now())
+
+#print(today.strftime("%A, %B %dth %Y") + now.strftime(" at %I:M%P"))
+print(today.strftime("%A, %B %dth %Y"))
+print(now.strftime(" at %I:M%P"))
