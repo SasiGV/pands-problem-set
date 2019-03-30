df2d6421744ee7fd9eafe7c86bcb0298fe7b6208
Sasikala <g00376470@gmit.ie>
2019-03-30 14:19:36 +0000

    Udpated program sumupto

--- sumupto.py
+++ sumupto.py
@@ -1,13 +1,16 @@
 #Program to calculate sum of numbers from 1 to the number entered
 
 #Get input from User
-n=int(input("Enter an Integer Number to calculate Sum"))
+n=int(input("Please enter a positive integer "))
 
 #Create an empty intergar called Sum and set initialie it to zero
 sum = 0
 
-#Create a For look and add number starts from 1 upto the number entered
-for num in range (0, n+1, 1):
-    sum = sum + num 
-#Print the output value    
-print ("Sum of first ", n, "number is: ", sum)
+if n > 0:
+    #Create a For look and add number starts from 1 upto the number entered
+    for num in range (0, n+1, 1):
+        sum = sum + num
+    #Print the output value    
+    print ("Sum of numbers 1 to", n, "is: ", sum)
+else:
+    print ("Oops, It is a negative number") 
