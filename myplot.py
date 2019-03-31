61f2eba9b2d888d1453a2d43151d247bec8740c3
Sasikala <g00376470@gmit.ie>
2019-03-31 02:59:37 +0100

    Added myplot program

---
+++ myplot.py
@@ -0,0 +1,27 @@
+#Sasikala Varatharajan - G00376470
+
+#Program to print plot of functions x, x2 and 2x in the range[0, 4]
+
+#Import numpy arrays to  store the range values
+#Import Matplot library to use matplot functions 
+import numpy as np
+import matplotlib.pyplot as plt
+
+#Arange range values
+x = np.arange(0,4)
+
+#Define different variables for the given functions
+y = x
+y1 = x**2
+y2 = 2**x
+
+#Arange the values in a plot
+plt.plot(x, y, x, y1, x, y2, '.-')
+
+#Label x-axis and y-axis 
+plt.xlabel('Values of X')
+plt.ylabel('Values of Y')
+plt.title('Histogram of functions x, x^2 and 2^x')
+plt.grid(True)
+
+plt.show()
